b898fa4c17355dd72355b7804d0ff3eb2675f64a
sugaken <kenta.suga@outlook.jp>
2023-07-24 23:27:35 +0900

    first commit

---
+++ src/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Fullstack Project</title>
+    @viteReactRefresh
+    @vite(['resources/sass/app.scss', 'resources/ts/app.ts'])
+</head>
+<body>
+    <div id="example"></div>
+</body>
+</html>
---
+++ src/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    server: {
+        hmr: {
+            host: 'localhost',
+        }
+    },
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.ts'
+            ],
+            refresh: true,
+        }),
+        react(),
+    ],
+    resolve: {
+        alias: {
+            '@': '/resources/ts',
+        },
+    },
+});
